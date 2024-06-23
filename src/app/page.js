68939d4924eb01079ed46e29d5e97333bec8f795
Andrey <devandrey98@gmail.com>
2024-06-23 15:24:45 -0600

    added logic to save elements in different scopes when dragged n dropped

--- src/app/page.js
+++ src/app/page.js
@@ -21,10 +21,78 @@ export default function Home() {
     setCompletedTasks
   }
 
+  const handleOnDragEnd = (result) => {    
+    if (!result.destination) {
+      return;
+    }
+    const source = result.source.droppableId
+    const destination = result.destination.droppableId
+    
+    const idToSearch = result.draggableId
+    
+    const sourceTasksFromLS = JSON.parse(localStorage.getItem(source));
+    let tempArr = [];
+
+    let elementToMove;
+
+
+    sourceTasksFromLS.forEach(element => {
+      if(element.id == idToSearch){
+        elementToMove = element;
+
+        tempArr = sourceTasksFromLS.filter((el)=>{ return el.id != idToSearch});
+
+      }
+    });
+    localStorage.setItem(source,JSON.stringify(tempArr));
+
+    //eliminamos el elemento del source
+    if(source == "inProgressTasks"){
+      setInProgressTasks(tempArr);
+    }else if(source == "newTasks"){
+      setNewTasks(tempArr);
+    }      
+    else if(source == "completedTasks"){
+      setCompletedTasks(tempArr);
+    }
+
+    // agregamos el elemento al array destination
+    const isDestinationInLocalS = JSON.parse(localStorage.getItem(destination));
+
+    if(isDestinationInLocalS && isDestinationInLocalS.length>0){
+
+      isDestinationInLocalS.push(elementToMove);
+      localStorage.setItem(destination,JSON.stringify(isDestinationInLocalS));
+      if(destination == "inProgressTasks"){
+        setInProgressTasks(isDestinationInLocalS);
+      }else if(destination == "newTasks"){
+        setNewTasks(isDestinationInLocalS);
+        
+      }else if(destination == "completedTasks"){
+        setCompletedTasks(isDestinationInLocalS);
+
+      }
+      
+    }else{
+      let temp = [elementToMove];
+      localStorage.setItem(destination,JSON.stringify(temp));
+      if(destination == "inProgressTasks"){
+        setInProgressTasks(temp);
+      }else if(destination == "newTasks"){
+        setNewTasks(temp);
+        
+      }else if(destination == "completedTasks"){
+        setCompletedTasks(temp);
+
+      }
+    }
+    // TODO: fix this spaghetti
+
+  }
   return (
   <Context.Provider value={contextValues}>
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4 bg-gray-50 relative">
-        <DragDropContext >
+        <DragDropContext onDragEnd={handleOnDragEnd}>
           <TaskForm tasks={newTasks} setTasks={setNewTasks}/>
           <TasksContainer  setEditClicked={setEditClicked} />
         </DragDropContext>
