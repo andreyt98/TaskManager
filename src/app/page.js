13d3578a6be17fdc1b24b30dfaef822f6240583e
Andrey <devandrey98@gmail.com>
2024-06-23 15:22:45 -0600

    added structure to drag components

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@
 import { useState } from "react";
 import { TaskForm } from "../components/TaskForm";
 import { TasksContainer } from "@/components/TasksContainer";
-import { DragDropContext,Droppable } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd";
+import { Context } from "@/context/Context";
 
 
 export default function Home() {
@@ -10,30 +11,24 @@ export default function Home() {
   const [inProgresstasks, setInProgressTasks] = useState(JSON.parse(localStorage.getItem("inProgressTasks")) || []);
   const [completedTasks, setCompletedTasks] = useState(JSON.parse(localStorage.getItem("completedTasks")) || []);
   const [editClicked, setEditClicked] = useState(false);
-  const [clickedId, setClickedId] = useState(false);
-  
-  const handleOnDragEnd = (result) => {    
-    //console.log(result)
-  }
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4 bg-gray-50 relative">
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <TaskForm tasks={newTasks} setTasks={setNewTasks}/>
-        <Droppable droppableId="tasksSections" >
-          {(provided)=>{
 
-            return(
-              <div ref={provided.innerRef} {...provided.droppableProps} className="p-2 flex flex-col justify-center lg:flex-row gap-8 lg:w-11/12 xl:w-9/12 w-full">
-                {/* TODO: use centralized state to avoid passing too many props */}
-                <TasksContainer  tasks={newTasks} setTasks={setNewTasks} setEditClicked={setEditClicked}/>
-                <span className="absolute">
-                  {provided.placeholder}
-                </span>
-              </div>
-            )
-          }}
-        </Droppable>
-      </DragDropContext>
-    </main>
+  const contextValues = {
+    newTasks, 
+    setNewTasks,
+    inProgresstasks,
+    setInProgressTasks,
+    completedTasks,
+    setCompletedTasks
+  }
+
+  return (
+  <Context.Provider value={contextValues}>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4 bg-gray-50 relative">
+        <DragDropContext >
+          <TaskForm tasks={newTasks} setTasks={setNewTasks}/>
+          <TasksContainer  setEditClicked={setEditClicked} />
+        </DragDropContext>
+      </main>
+  </Context.Provider>
   );
 }
--- src/components/TasksContainer.jsx
+++ src/components/TasksContainer.jsx
@@ -1,24 +1,30 @@
 "use client";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 import Tasks from "./Tasks";
-export function TasksContainer({ tasks, setTasks, setEditClicked }) {
+import { useContext } from "react";
+import { Context } from "@/context/Context";
+
+export function TasksContainer({ setEditClicked }) {
+
+  const { newTasks, setNewTasks, inProgresstasks, setInProgressTasks, completedTasks, setCompletedTasks } = useContext(Context);
+
   return (
-    <>
-    {/* TODO: create single component to render droppable elements to be able to render any array  */}
-      <Droppable droppableId="tasksSections">
+    <div className="p-2 flex flex-col justify-center lg:flex-row gap-8 lg:w-11/12 xl:w-9/12 w-full">
+      {/* TODO: create single component to render droppable elements   */}
+      <Droppable droppableId="newTasks">
         {(provided) => {
           return (
             <div ref={provided.innerRef} {...provided.droppableProps} className="min-h-48 flex-1 border  rounded-lg px-4 py-8 w-full flex flex-col gap-4 shadow-lg max-h-96 overflow-auto">
               <p className="bg-gray-100 text-orange-700 text-xs inline-flex items-center px-2.5 py-0.5 rounded-md font-semibold border-l-8 border-l-orange-600">New tasks</p>
-              {tasks &&
-                tasks.length > 0 &&
-                tasks.map((task, index) => {
+              {newTasks &&
+                newTasks.length > 0 &&
+                newTasks.map((task, index) => {
                   return (
                     <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
                       {(provided) => {
                         return (
                           <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
-                            <Tasks tasks={tasks} setTasks={setTasks} task={task} setEditClicked={setEditClicked} key={task.id} />
+                            <Tasks tasks={newTasks} setTasks={setNewTasks} task={task} setEditClicked={setEditClicked} key={task.id} />
                           </div>
                         );
                       }}
@@ -31,27 +37,57 @@ export function TasksContainer({ tasks, setTasks, setEditClicked }) {
         }}
       </Droppable>
 
-      <Droppable droppableId="tasksSections2">
+      <Droppable droppableId="inProgressTasks">
         {(provided) => {
           return (
             <div ref={provided.innerRef} {...provided.droppableProps} className="min-h-48 flex-1 border  rounded-lg px-4 py-8 w-full flex flex-col gap-4 shadow-lg max-h-lg overflow-auto">
               <p className="bg-gray-100 text-yellow-600 text-xs  inline-flex items-center px-2.5 py-0.5 rounded-md font-semibold border-l-8 border-l-yellow-500">In progress</p>
+              {inProgresstasks &&
+                inProgresstasks.length > 0 &&
+                inProgresstasks.map((task, index) => {
+                  return (
+                    <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                      {(provided) => {
+                        return (
+                          <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                            <Tasks tasks={inProgresstasks} setTasks={setInProgressTasks} task={task} setEditClicked={setEditClicked} key={task.id} />
+                          </div>
+                        );
+                      }}
+                    </Draggable>
+                  );
+                })}
               <span className="absolute">{provided.placeholder}</span>
             </div>
           );
         }}
       </Droppable>
 
-      <Droppable droppableId="tasksSections3">
+      <Droppable droppableId="completedTasks">
         {(provided) => {
           return (
             <div ref={provided.innerRef} {...provided.droppableProps} className="min-h-48 flex-1 border  rounded-lg px-4 py-8 w-full flex flex-col gap-4 shadow-lg max-h-lg overflow-auto">
               <p className="bg-gray-100 text-green-600 text-xs  inline-flex items-center px-2.5 py-0.5 rounded-md font-semibold border-l-8 border-l-green-600">Completed</p>
+              {completedTasks &&
+                completedTasks.length > 0 &&
+                completedTasks.map((task, index) => {
+                  return (
+                    <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                      {(provided) => {
+                        return (
+                          <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                            <Tasks tasks={completedTasks} setTasks={setCompletedTasks} task={task} setEditClicked={setEditClicked} key={task.id} />
+                          </div>
+                        );
+                      }}
+                    </Draggable>
+                  );
+                })}
               <span className="absolute">{provided.placeholder}</span>
             </div>
           );
         }}
       </Droppable>
-    </>
+    </div>
   );
 }
