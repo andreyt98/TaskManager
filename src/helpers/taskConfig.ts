68e6e37851e0d922160180a8fda18bbe6e65310e
Andrey <devandrey98@gmail.com>
2024-06-28 07:13:59 -0600

    feat: added more categories

--- src/helpers/taskConfig.ts
+++ src/helpers/taskConfig.ts
@@ -1,6 +1,6 @@
 import { typeOfTaskObject } from "./submitTask";
 
-export const categories =['Work','School','Family','Pet','Workout','Friends','Events','No category'];
+export const categories = ["Work", "School", "Family", "Pet", "Chill time", "Hobbies", "Business", "Chores", "Study", "Errands", "Workout", "Friends", "Events","Vacations", "No category"];
 
 export const saveToLocalStorage = (nameOfItem: string, arrayToSave: typeOfTaskObject[]) => {
     localStorage.setItem(nameOfItem, JSON.stringify(arrayToSave));
