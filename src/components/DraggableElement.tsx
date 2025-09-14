6c11ec3d527058df5b76c85d505c5fb27d745801
Andrey <devandrey98@gmail.com>
2025-09-14 17:19:02 -0600

    refact: use task status component

--- src/components/DraggableElement.tsx
+++ src/components/DraggableElement.tsx
@@ -3,24 +3,20 @@ import { Draggable, DroppableProvided } from "react-beautiful-dnd";
 import { typeOfTaskObject } from "../helpers/submitTask";
 import React, { Dispatch, SetStateAction } from "react";
 import Tasks from "./Tasks";
+import { taskStatusType } from "../Types/taskStatus";
+import TaskStatus from "./Task/TaskStatus";
 
 interface IDraggableElement {
   provided: DroppableProvided;
   taskArray: typeOfTaskObject[];
   setTasks: Dispatch<SetStateAction<typeOfTaskObject[]>>;
-  typeOfArrayName: string;
+  typeOfArrayName: taskStatusType;
 }
 
 export const DraggableElement: React.FC<IDraggableElement> = ({ provided, typeOfArrayName, taskArray, setTasks }) => {
   return (
     <div ref={provided.innerRef} {...provided.droppableProps} className="min-h-48 flex-1 border  rounded-lg px-4 py-8 w-full flex flex-col gap-4 shadow-lg max-h-96 overflow-auto">
-      <p
-        className={`bg-gray-100 ${
-          typeOfArrayName == "New tasks" ? `text-orange-700 border-l-orange-600` : typeOfArrayName == "In progress" ? "text-yellow-600  border-l-yellow-500" : " text-green-600 border-l-green-600"
-        } text-xs inline-flex items-center px-2.5 py-0.5 rounded-md font-semibold border-l-8 `}
-      >
-        {typeOfArrayName}
-      </p>
+      <TaskStatus status={typeOfArrayName} />
       {taskArray &&
         taskArray.length > 0 &&
         taskArray.map((task, index) => {
