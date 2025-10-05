8df6f0a69fce38ec54cde9342209fc28b97216d4
Andrey <devandrey98@gmail.com>
2025-10-05 14:33:34 -0600

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
-  title: "Task Manager",
+  title: "Clarity",
   description: "Manage your daily tasks!",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
